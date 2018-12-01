7f78d41cfdc0c976bf3c3c4b5b2c5fba9de9ce21
TriDivide <liamsmitha@gmail.com>
2018-12-01 11:03:58 +0000

    - built the player object

--- Assets/DataModel/Player.cs
+++ Assets/DataModel/Player.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class Player : MonoBehaviour {
+public class Player  {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum ClassType {
+        Fighter,
+        Defender,
+        Magician
+    }
+
+    ClassType playerClass;
+
+    float health = 100f;
+    float magic = 100f;
+    List<System.Object> powerUps = new List<System.Object>();
+    float attackStat = 10f;
+    float defenceStat = 10f;
+    float magicStat = 10f;
+    List<System.Object> invenvory = new List<System.Object>();
+    String name = "";
+
+   
+     
+
+
+
+    
+    
 }
