ea09c0e4023267ff4e19bb75215e6f70c5e75eff
TriDivide <liamsmitha@gmail.com>
2018-12-01 17:31:46 +0000

    - got player movement working

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour {
+
+
+    public float movementSpeed = 1f;
+    private Vector2 movementVelocity;
+    private Rigidbody2D playerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        movementVelocity = moveInput.normalized * movementSpeed;
+        
+ 
+
+        if (Input.GetKeyDown("space")) {
+            print("space was pressed");
+        }
+
+        if (Input.GetKeyDown("e")) {
+            print("e was pressed");
+        }
+
+	}
+
+    void FixedUpdate() {
+        playerRigidBody.MovePosition(playerRigidBody.position + movementVelocity * Time.fixedDeltaTime);
+
+    }
+}
